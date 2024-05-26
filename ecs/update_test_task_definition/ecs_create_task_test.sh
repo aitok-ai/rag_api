a34d40b17d3785bc8859ae03067930bd06aafcc8
Shi Jin <jinzishuai@gmail.com>
2024-05-25 22:20:03 -0600

    Script to create the test task definition

---
+++ ecs/update_test_task_definition/ecs_create_task_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# source ./test_variables.sh
+
+# Create a new version in the same family (if exists)
+aws ecs register-task-definition --cli-input-json file://task_definition_test.jsonß
