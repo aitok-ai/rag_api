a579235aab1e1c9c951915a62a66658c9bdc6d2b
Shi Jin <jinzishuai@gmail.com>
2024-05-25 22:28:26 -0600

    remove typo

--- ecs/update_test_task_definition/ecs_create_task_test.sh
+++ ecs/update_test_task_definition/ecs_create_task_test.sh
@@ -3,4 +3,4 @@
 # source ./test_variables.sh
 
 # Create a new version in the same family (if exists)
-aws ecs register-task-definition --cli-input-json file://task_definition_test.jsonß
+aws ecs register-task-definition --cli-input-json file://task_definition_test.json
