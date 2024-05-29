2a2a7ea8e55907e9aef4832b415d4677d5dde487
Shi Jin <jinzishuai@gmail.com>
2024-05-29 14:17:21 -0600

    update production parameters for ECS

--- ecs/update_prod_task_definition/ecs_create_task_prod.sh
+++ ecs/update_prod_task_definition/ecs_create_task_prod.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# source ./test_variables.sh
-ECS_CLUSTER_NAME=ec2-cluster
-ECS_SERVICE_NAME=libreChat-rag-api-test-service
-ECS_TASK_NAME=libreChat-rag-api-test
+ECS_CLUSTER_NAME=production
+ECS_SERVICE_NAME=libreChat-rag-api-prod-service
+ECS_TASK_NAME=libreChat-rag-api-prod
 
 # Create a new version in the same family (if exists)
-aws ecs register-task-definition --cli-input-json file://task_definition_test.json
+aws ecs register-task-definition --cli-input-json file://task_definition_prod.json
 
 # Force restart ECS service 
 aws ecs update-service \
