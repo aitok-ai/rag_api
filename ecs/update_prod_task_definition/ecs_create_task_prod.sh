48435fddb4721dd0837567f106ad08c7d57fbad8
Shi Jin <jinzishuai@gmail.com>
2024-05-29 14:30:17 -0600

    switch on production ECS in Singapore (#4)
    * Add files to set up RAG in Production
    
    * update production parameters for ECS
    
    * overwrite the region for production
    
    * also set the AWS_REGION

---
+++ ecs/update_prod_task_definition/ecs_create_task_prod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export AWS_DEFAULT_REGION=ap-southeast-1
+export AWS_REGION=ap-southeast-1
+
+ECS_CLUSTER_NAME=production
+ECS_SERVICE_NAME=libreChat-rag-api-prod-service
+ECS_TASK_NAME=libreChat-rag-api-prod
+
+# Create a new version in the same family (if exists)
+aws ecs register-task-definition --cli-input-json file://task_definition_prod.json
+
+# Force restart ECS service 
+aws ecs update-service \
+    --cluster $ECS_CLUSTER_NAME \
+    --service $ECS_SERVICE_NAME \
+    --task-definition $ECS_TASK_NAME \
+    --force-new-deployment
